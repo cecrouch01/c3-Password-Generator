93fcf470c181e002647ad874c76bd32449eaf571
Caleb Crouch <cecrouch01@gmail.com>
2023-06-19 00:39:30 -0400

    fixing prompt tree

--- script.js
+++ script.js
@@ -1,50 +1,64 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// var lengthValue = pwLength
+// var ucValue = charCodeAt() //reference 65-90 (uppercase) and 97-122 (lowercase)
+// var numValue = [1,2,3,4,5,6,7,8,9,0] //reference 48-57
+// var specValue = charCodeat() //33, 35-38, 40-47, 58-64, 91-95
+
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var specialCharacters = "!@#$%^&*()"
+var characterBank = ""
+var randomIndex = Math.floor(Math.random() * upperCase.length);
+
+console.log(upperCase[randomIndex])
+console.log(upperCase.length)
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+ // Prompt for character length
+  var pwLength = window.prompt ("How many characters would you like to use for you password?");
+   if (pwLength > 8 && pwLength < 128) {
+    window.alert ("Hurray");
+    } else {
+    window.alert ("Boo!")
+      writePassword();
+    } //return lengthValue
+
+//Prompt for including Lowercase Characters
+  var lcInclusion = window.confirm ("Would you like to include Uppercase Characters in your password?")
+  
+//Prompt for including Uppercase characters
+  var ucInclusion = window.confirm ("Would you like to include Uppercase Characters in your password?"); 
+    console.log (ucInclusion)
+
+
+//Prompt for including Numbers 
+  var numInclusion = window.confirm ("Would you like to include Numbers in your password?")
+    console.log(numInclusion)
+
+  
+//Prompt for including Special characters
+  var specInclusion = window.confirm ("Would you like to include special characters in you password?");
+    console.log(specInclusion)
+  
+//if ucinclusion, numinclusion, specinclusion i must use uppercase numbers and special characters in my password of pwlength
+
+
   var passwordText = document.querySelector("#password");
    
   passwordText.value = password;
 }
 
+
 // Add event listener to generate button
-//I replaced writePassword with this function
-generateBtn.addEventListener("click", function(){
-  var passwordCharacters = window.prompt("How many characters would you like to use for your password?");
-    if (passwordCharacters < 8){
-    window.alert("Please make your password between 8 and 128 characters")
-    } else if (passwordCharacters > 128){
-      window.alert("Please make your password between 8 and 128 characters")
-    } else {{var uppercaseInclusionOne = window.confirm ("Would you like to use both Uppercase and Lowercase Letters in your password?");
-        if (uppercaseInclusionOne === true){var numericalInclusionOne =
-          window.confirm("Would you like to use numbers in your password?");
-            if (numericalInclusionOne === true){
-              var specialCharInclusionOne = window.confirm ("Would you like to include special characters?");
-                if (specialCharInclusionOne === true){
-                  window.alert("Thank you!")
-                } else {window.alert("Thank you!")}
-            } else {var specialCharInclusionTwo = window.confirm ("Would you like to include special characters?");
-              if (specialCharInclusionTwo === true){
-                window.alert("Thank you!")
-              } else {window.alert("Thank you!")}}
-        } else {var numbericalInclusionTwo = window.confirm("Would you like to use numbers in your password?");
-        if (numbericalInclusionTwo === true){
-          var specialCharInclusionThree = window.confirm ("Would you like to include special characters?");
-            if (specialCharInclusionThree === true) {
-              window.alert("Thank you!");
-            } else {window.alert("Thank you!")}
-        } else {var specialInclusionFour = window.confirm("Would you like to include special characters?");
-          if (specialInclusionFour === true) {
-            window.alert("Thank you!")
-          } else {window.alert("Thank you!")}
-      }}}}
-    });
-//uppercase/lowercase, numeric, special characters
+generateBtn.addEventListener("click", writePassword);
 //Click button prompt appears
 //I have a series of prompts for password criteria
 //userpicks the critera
 //there is a minimum of 8 and maxium of 128 characters
 //confirmation of whether or not to include lowercase, uppercase, numberic, and/or special characters
 //when all prompts are answered then a password is generated. 
+
+
+
+
