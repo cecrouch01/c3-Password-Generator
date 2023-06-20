814970d60ad860578273e542e1bbdfc232a13c69
Caleb Crouch <cecrouch01@gmail.com>
2023-06-19 20:58:26 -0400

    Character Bank Generation

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numericalCharacters = "1234567890"
 var specialCharacters = "!@#$%^&*()"
 var characterBank = ""
+console.log(characterBank)
+console.log(characterBank.concat(lowercaseCharacters))
 
 // Write password to the #password input
 function writePassword() {
@@ -23,42 +25,55 @@ function writePassword() {
 
 //Prompt for including Lowercase Characters
   var lcInclusion = window.confirm ("Would you like to include Lowercase Characters in your password?");
+    if (lcInclusion === true) {
+      characterBank = characterBank.concat(lowercaseCharacters);
+      console.log(lcInclusion)
+    }
+
   
-
 //Prompt for including Uppercase characters
-  var ucInclusion = window.confirm ("Would you like to include Uppercase Characters in your password?"); 
-    console.log(ucInclusion)
-
+  var ucInclusion = window.confirm ("Would you like to include Uppercase Characters in your password?");
+    if (ucInclusion === true) {
+     characterBank = characterBank.concat(uppercaseCharacters);
+      console.log(ucInclusion)
+    }
+  
 
 //Prompt for including Numbers 
   var numInclusion = window.confirm ("Would you like to include Numbers in your password?");
-    console.log(numInclusion)
+    if (numInclusion === true) {
+      characterBank = characterBank.concat(numericalCharacters)
+      console.log(numInclusion);
+    }
 
-  
 //Prompt for including Special characters
   var specInclusion = window.confirm ("Would you like to include Special Characters in you password?");
-    console.log(specInclusion)
+    if (specInclusion === true) {
+     characterBank = characterBank.concat(specialCharacters);
+      console.log(specInclusion);
+    }
+
+  //if values are not true
+    var password = ""
+    if (lcInclusion || ucInclusion || numInclusion || specInclusion) {
+      for (var i = 1; i < pwLength; i++) {
+        var pwCharacter = String.characterBank[Math.floor(Math.random() * characterBank.length)]
+       password = password.concat(pwCharacter)
+      } 
+    } else {
+      window.alert("Please use at least one character type for you password");
+      writePassword();
+    }
+    console.log(password)
   
+
+
     //First I need to create a bank of characters. For that bank if ucinclusion, numinclusion,and/or specinclusion true then I must use uppercase numbers and special characters in my password of pwlength.
     //Second I need to generate multiple random characters of a specified length.
     // Third I need to make sure the generated password is displayed to the webpage
-      if (lcInclusion === true){
-        characterBank.concat(lowercaseCharacters);
-        }
     
-    //   if (ucInclusion === true) {
-    //     characterBank.concat(uppercaseCharacters);
-    //     } 
-    
-    //   if (numInclusion === true) {
-    //     characterBank.concat(numericalCharacters);
-    //     }
-    
-    //   if (specInclusion === true) {
-    //     characterBank.concat(specialCharacters);
-    //     }
-    // }
-    // console.log(characterBank)
+    //console.log(characterBank) THIS ISN'T WORKING!
+
 
     
 
@@ -85,7 +100,3 @@ generateBtn.addEventListener("click", writePassword);
 //there is a minimum of 8 and maxium of 128 characters
 //confirmation of whether or not to include lowercase, uppercase, numberic, and/or special characters
 //when all prompts are answered then a password is generated. 
-
-
-
-
